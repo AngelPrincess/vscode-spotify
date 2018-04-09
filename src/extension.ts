a4333c428d30619939df68d2b607ed5f6f35c15b
JLee <soyumatic@gmail.com>
2018-04-09 17:02:18 -0400

    init

---
+++ src/extension.ts
@@ -0,0 +1,17 @@
+import {ExtensionContext} from 'vscode';
+import {createCommands} from './Commands';
+import {SpotifyStatus} from './SpotifyStatus';
+import {SpotifyStatusController} from './SpotifyStatusController';
+
+// This method is called when your extension is activated. Activation is
+// controlled by the activation events defined in package.json.
+export function activate(context: ExtensionContext) {    
+    // This line of code will only be executed once when your extension is activated.        
+    let spotifyStatus = new SpotifyStatus();
+    let controller = new SpotifyStatusController(spotifyStatus, context.globalState);
+
+    // Add to a list of disposables which are disposed when this extension is deactivated.
+    context.subscriptions.push(controller);
+    context.subscriptions.push(spotifyStatus);
+    context.subscriptions.push(createCommands(spotifyStatus, controller));
+}
